21b06cd28332769277c38af71e42bf0aa2b25b40
Gabriel Gomes Nicolim <gabriel.nicolim@unesp.br>
2020-10-01 08:19:59 -0300

    Add files via upload

---
+++ Programas/Básico/Tipos_Primitivos.py
@@ -0,0 +1,8 @@
+n1 = input('Digite um valor: ')
+print(type(n1))  # Type() => Retorna o tipo da variavel
+n1 = int(input('Digite um valor: '))
+# int(input()) => Converte o valor recebido pelo input para int
+print(type(n1))
+
+
+
