3cea04871de7f453d66e05e026f717d5cacdd836
Gabriel Gomes Nicolim <gabriel.nicolim@unesp.br>
2020-10-04 12:04:25 -0300

    Add files via upload

---
+++ Programas/Exercícios/Math/Raiz_Quadrada.py
@@ -0,0 +1,5 @@
+# Importa a biblioteca math
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num) # Encontra a raiz quadrada de num
+print('A raiz de {} é {:.2f}'.format(num, raiz))
