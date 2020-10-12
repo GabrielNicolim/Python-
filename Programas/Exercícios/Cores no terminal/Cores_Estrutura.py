c933e6e31aa87453a873c608783d02a2745deff7
Gabriel <gabriel.nicolim@unesp.br>
2020-10-11 23:44:20 -0300

    Arquivo removido

--- Programas/Exercícios/Cores no terminal/Cores_Estrutura.py
+++
@@ -1,8 +0,0 @@
-nome = str(input('Digite seu nome: '))
-
-cores = {'limpa': '\033[m',
-         'azul': '\033[34m',
-         'amarelo': '\033[33m'
-         }
-
-print('Olá, {}{}{}!'.format(cores['azul'], nome, cores['limpa']))
