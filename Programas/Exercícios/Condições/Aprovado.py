63d358f211fdf0a0060c322be30726ade36df8e3
Gabriel Gomes Nicolim <gabriel.nicolim@unesp.br>
2020-10-14 01:59:36 -0300

    Add files via upload

---
+++ Programas/Exercícios/Condições/Aprovado.py
@@ -0,0 +1,9 @@
+A = float(input('\033[33mDigite a média do aluno: '))
+
+if A >= 7:
+    print('Aluno Aprovado!')
+elif A >= 5:
+    print('Aluno de Recuperação')
+else:
+    print('Aluno Reprovado')
+
