eca764b202f918a059776ee41b35e30c324c93fc
Gabriel Gomes Nicolim <gabriel.nicolim@unesp.br>
2020-10-21 12:53:39 -0300

    Add files via upload

---
+++ Scripts/Exercícios/NepsAcademy/Premio_do_Milhao.py
@@ -0,0 +1,21 @@
+# Recebe e armazena valores
+
+n = int(input())
+
+visualizacoes = []
+for i in range(n):
+    a = int(input())
+    visualizacoes.append(a)
+
+# Processamento
+
+total = 0
+resposta = -1
+
+for i, v in enumerate(visualizacoes):  # Retorna índice para i e o valor indexado para v
+    dia = i + 1
+    total += v
+    if total >= 1000000 and resposta == -1:
+        resposta = dia
+
+print(resposta)
