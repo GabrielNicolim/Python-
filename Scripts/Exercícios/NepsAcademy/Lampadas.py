3f29491f91c1552d8a9131e8ff57386a51330d8f
Gabriel Gomes Nicolim <gabriel.nicolim@unesp.br>
2020-10-21 16:44:24 -0300

    Add files via upload

---
+++ Scripts/Exercícios/NepsAcademy/Lampadas.py
@@ -0,0 +1,30 @@
+# Exercício retirado de: https://neps.academy/lessonn
+
+n = int(input())
+l = input().split()
+
+# Lampadas em estado inicial (apagadas)
+
+a = 0
+b = 0
+
+for acao in l:
+    if acao == '1':
+        if a == 1:
+            a = 0
+        else:
+            a = 1
+    elif acao == '2':
+        if a == 1:
+            a = 0
+        else:
+            a = 1
+
+        if b == 1:
+            b = 0
+        else:
+            b = 1
+
+print(a)
+print(b)
+
